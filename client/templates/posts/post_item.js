de072f2924a8910244e3aad8048c9e113192c8d7
micmcc <micmcc@outlook.com>
2015-04-26 22:46:35 -0600

    Chapter 3

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mccm on 26/04/15.
+ */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,20 @@
+/**
+ * Created by mccm on 26/04/15.
+ */
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    }
+];
+Template.postsList.helpers({
+    posts: postsData
+});
