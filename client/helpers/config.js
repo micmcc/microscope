b8d323af555ff9dea9f594428e27f92b7b096eae
micmcc <micmcc@outlook.com>
2015-04-29 20:34:51 -0600

    Added Authentication and Users

---
+++ client/helpers/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by mccm on 29/04/15.
+ */
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+});
---
+++ client/templates/application/layout.js
@@ -0,0 +1,15 @@
+/**
+ * Created by mccm on 29/04/15.
+ */
+Template.layout.helpers({
+
+        pageTitle: function () {
+            if(Session.get('pageTitle'))
+            {
+                return Session.get('pageTitle');
+            }
+            else {
+                return 'Microscope';
+            }
+        }
+});
