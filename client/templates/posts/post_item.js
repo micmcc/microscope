8b0a71720614f37c0a6cf2580d090043eda123c3
micmcc <micmcc@outlook.com>
2015-05-15 21:02:26 -0600

    Added edit/delete post

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,6 +2,10 @@
  * Created by mccm on 26/04/15.
  */
 Template.postItem.helpers({
+    ownPost: function(){
+        return this.userId === Meteor.userId();
+    },
+
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
