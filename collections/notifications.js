76ef494d41acb3e93f5fe6517c0a0abc865bfc47
micmcc <micmcc@outlook.com>
2015-05-19 23:15:57 -0600

    Added notifications on posts

--- collections/notifications.js
+++ collections/notifications.js
@@ -2,11 +2,23 @@ Notifications = new Mongo.Collection('notifications');
 
 Notifications.allow ({
 	update: function (userId, doc, fieldNames){
-		return ownsDocument(userId, doc) && 
+		//check that the user owns the notification being modified
+		//			 the user is only trying to update a single filed
+		//			 the single filed is the read property of out notifications
+		return ownsDocument(userId, doc) &&
 			fieldNames.length === 1 && fieldNames[0] === 'read';
 	}
 });
 
 createCommentNotification = function (comment){
-	
+	var post = Posts.findOne(comment.postId);
+	if (comment.userId !== post.userId){
+		Notifications.insert({
+			userId: post.userId,
+			postId: post._id,
+			commentId: comment._id,
+			commenterName: comment.author,
+			read: false
+		});
+	}
 }
--- collections/publications.js
+++ collections/publications.js
@@ -10,3 +10,7 @@ Meteor.publish('comments', function(postId) {
 
     return Comments.find({postId: postId});
 });
+
+Meteor.publish('notifications', function() {
+        return Notifications.find({userId: this.userId, read: false});
+});
